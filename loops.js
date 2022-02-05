42fced82f7585d6b7f1f092a11ca99483f5dc449
morahm <72052463+morahm@users.noreply.github.com>
2022-02-04 21:02:58 -0500

    initial commit

---
+++ loops.js
@@ -0,0 +1,34 @@
+for(var i = 1; i <= 20; i++) {
+    if(i % 2 == 1) console.log(i);
+}
+
+console.log("========= NEXT CODE =============")
+
+for(var i = 100; i >= 0; i--) {
+    if(i % 3 == 0) console.log(i);
+}
+
+console.log("========= NEXT CODE =============");
+
+for(var i = 4; i >= -3.5; i -= 1.5) {
+    console.log(i);
+}
+
+console.log("========= NEXT CODE =============");
+
+var sum = 0;
+for(var i = 1; i <= 100; i++){
+    sum += i;
+}
+console.log(sum);
+
+console.log("========= NEXT CODE =============");
+
+var product = 1;
+for(var i = 1; i <= 12; i++) {
+    product *= i;
+}
+console.log(product);
+
+
+
